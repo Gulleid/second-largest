c8dbae81419306e00aba78d9f8109b1ce22abc8f
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 14:48:03 +0300

    secondlargest

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function SecondLargest(arr){
+    let first = -1 , second = -1;
+
+    for(let i = 0; i <= arr.length-1; i++){
+        if(arr[i] > first){
+            second = first;
+            first = arr[i];
+        }
+        else if( arr[i] > second && arr[i] != first){
+            second = arr[i];
+        }
+    }
+    console.log(second);
+}
+let arr = [23, 66, 50, 12,  6]
+ SecondLargest(arr);
